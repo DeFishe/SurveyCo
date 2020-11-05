20e6800f7f0f3bbb4918f1c023f1a53faff442e1
Darian Fisher <darian.edward.fisher@outlook.com>
2020-11-05 14:27:01 -0500

    Improved Survey Functionality
    Expanded survey functionality. Combined view model allows for theoretically limitless survey questions.

--- Controllers/QuestionnaireController.cs
+++ Controllers/QuestionnaireController.cs
@@ -29,6 +29,35 @@ namespace SurveyCo.Controllers
 
         public IActionResult Answer()
         {
+            var questionnaireVM = new QuestionnaireViewModel();
+            var questions = from q in _context.Questions
+                            where q.QuestionnaireID == 1
+                            select q;
+            questionnaireVM.Questions = questions.ToList();
+            return View(questionnaireVM);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Answer(QuestionnaireViewModel questionnaireViewModel)
+        {
+            //TODO: Change to context sensetive
+            //for (int i = 0; i < questionnaireViewModel.Questions.Count; i++)
+            //{
+            //    questionnaireViewModel.Answers[i].QuestionID = 
+            //        questionnaireViewModel.Questions[i].QuestionID;
+            //}
+            questionnaireViewModel.Answers[0].QuestionID = 1;
+            questionnaireViewModel.Answers[1].QuestionID = 2;
+            questionnaireViewModel.Answers[2].QuestionID = 3;
+            questionnaireViewModel.Answers[3].QuestionID = 4;
+            _context.AddRange(questionnaireViewModel.Answers);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Result));
+        }
+
+        public IActionResult Result()
+        {
             return View();
         }
     }
---
+++ Models/QuestionnaireViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyCo.Models
+{
+    public class QuestionnaireViewModel
+    {
+        public List<Question> Questions { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+}
