850c220db316b2458f7e0fc551918ec1364e34ab
Darian Fisher <darian.edward.fisher@outlook.com>
2020-11-11 14:33:35 -0500

    Sample Question Creation
    Worked out a sample question creation process to practice

---
+++ Controllers/QuestionnaireInfoController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SurveyCo.Data;
+using SurveyCo.Models;
+
+namespace SurveyCo.Controllers
+{
+    public class QuestionnaireInfoController : Controller
+    {
+        private readonly SurveyCoDbContext _context;
+
+        public QuestionnaireInfoController(SurveyCoDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CreateQuestionnaireViewModel cqvm)
+        {
+            
+            //This is AWFUL. Fix this later.
+            Questionnaire questionnaire = new Questionnaire();
+            questionnaire.CompanyID = cqvm.CompanyID;  
+            _context.Add(questionnaire);
+            _context.SaveChanges();
+
+            var questionnaires = from m in _context.Questionnaires
+                          select m;
+            int newestQuestID = 0;
+            foreach (var item in questionnaires)
+            {
+                if (item.QuestionnaireID > newestQuestID)
+                {
+                    newestQuestID = item.QuestionnaireID;
+                }
+            }
+
+            foreach (var question in cqvm.Questions)
+            {
+                question.QuestionnaireID = newestQuestID;
+            }
+
+            _context.AddRange(cqvm.Questions);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Result));
+        }
+
+        public IActionResult Result()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ Models/CreateQuestionnaireViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyCo.Models
+{
+    public class CreateQuestionnaireViewModel
+    {
+        public int CompanyID { get; set; }
+
+        public List<Question> Questions { get; set; }
+    }
+}
